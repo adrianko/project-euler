520125bea0e6618cead6113d661247a1c352a236
Adrian Ko <adrianchko@gmail.com>
2014-11-03 15:32:35 +0000

    Continue P26

--- src/Problem26.java
+++ src/Problem26.java
@@ -5,15 +5,15 @@ import java.math.MathContext;
  * A unit fraction contains 1 in the numerator. The decimal representation of
  * the unit fractions with denominators 2 to 10 are given:
  *
- * 1/2	= 	0.5
- * 1/3	= 	0.(3)
- * 1/4	= 	0.25
- * 1/5	= 	0.2
- * 1/6	= 	0.1(6)
- * 1/7	= 	0.(142857)
- * 1/8	= 	0.125
- * 1/9	= 	0.(1)
- * 1/10	= 	0.1
+ * 1/2 = 0.5 
+ * 1/3 = 0.(3) 
+ * 1/4 = 0.25 
+ * 1/5 = 0.2 
+ * 1/6 = 0.1(6) 
+ * 1/7 = 0.(142857) 
+ * 1/8 = 0.125 
+ * 1/9 = 0.(1) 
+ * 1/10 = 0.1
  *
  * Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be
  * seen that 1/7 has a 6-digit recurring cycle.
@@ -23,32 +23,33 @@ import java.math.MathContext;
  */
 public class Problem26 {
 
-    public static void main(String[] args) {
-        for (int i = 1; i < 1000; i++) {
-            if (isPrime(i)) {
-                System.out.println(BigDecimal.valueOf(i).pow(-1, new MathContext(1000)));
-            }
-        }
-    }
+	public static void main(String[] args) {
+		for (int i = 1; i < 1000; i++) {
+			if (isPrime(i)) {
+				System.out.println(i + ": "
+						+ BigDecimal.valueOf(i).pow(-1, new MathContext(1000)));
+			}
+		}
+	}
 
-    public static boolean isPrime(int n) {
-        if (n < 2) {
-            return false;
-        } else if (n == 2 || n == 3) {
-            return true;
-        } else if (n % 2 == 0 || n % 3 == 0) {
-            return false;
-        }
+	public static boolean isPrime(int n) {
+		if (n < 2) {
+			return false;
+		} else if (n == 2 || n == 3) {
+			return true;
+		} else if (n % 2 == 0 || n % 3 == 0) {
+			return false;
+		}
 
-        long sqrtN = (long) Math.sqrt(n) + 1;
+		long sqrtN = (long) Math.sqrt(n) + 1;
 
-        for (long i = 6L; i <= sqrtN; i += 6) {
-            if (n % (i - 1) == 0 || n % (i + 1) == 0) {
-                return false;
-            }
-        }
+		for (long i = 6L; i <= sqrtN; i += 6) {
+			if (n % (i - 1) == 0 || n % (i + 1) == 0) {
+				return false;
+			}
+		}
 
-        return true;
-    }
+		return true;
+	}
 
 }
