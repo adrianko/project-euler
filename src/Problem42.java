8d694210790af3a569e83141bb9468b082e8e3ab
AK <adrianchko@gmail.com>
2015-02-21 18:50:48 +0000

    Find and collate scores

--- src/Problem42.java
+++ src/Problem42.java
@@ -1,3 +1,13 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * The nth term of the sequence of triangle numbers is given by, tn = ½n(n+1); so the first ten triangle numbers are:
  *
@@ -11,4 +21,35 @@
  * English words, how many are triangle words?
  */
 public class Problem42 {
+
+    public static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public static void main(String[] args) {
+        try {
+            Map<Integer, Set<String>> scores = new HashMap<>();
+
+            for (String w : readFile("external/problem42.txt").collect(Collectors.toList()).toString().split(",")) {
+                String word = w.replaceAll("(\\[|\\]|\")", "").trim().toLowerCase();
+                int score = 0;
+
+                for (char c : word.toCharArray()) {
+                    score += alphabet.indexOf(c) + 1;
+                }
+
+                if (!scores.containsKey(score)) {
+                    scores.put(score, new HashSet<>());
+                }
+
+                scores.get(score).add(word);
+            }
+
+            System.out.println(scores);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Stream<String> readFile(String filename) throws IOException {
+        return Files.lines(Paths.get(filename));
+    }
 }
