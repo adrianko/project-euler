b72a20321ec4bd8f7f782bf129cec750a882cf88
AK <adrianchko@gmail.com>
2015-02-22 15:08:25 +0000

    prime sieve with limit

--- src/Problem50.java
+++ src/Problem50.java
@@ -1,3 +1,6 @@
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * The prime 41, can be written as the sum of six consecutive primes:
  *
@@ -9,4 +12,34 @@
  * Which prime, below one-million, can be written as the sum of the most consecutive primes?
  */
 public class Problem50 {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static TreeSet<Integer> primeSieve(int limit) {
+        boolean[] numberList = new boolean[limit + 1];
+        TreeSet<Integer> primeList = new TreeSet<>();
+
+        for(int i = 2; i <= limit; i++) {
+            numberList[i] = true;
+        }
+
+        for(int i = 2; i * i <= limit; i++) {
+            if(numberList[i]) {
+                for(int j = i; i * j <= limit; j++) {
+                    numberList[i * j] = false;
+                }
+            }
+        }
+
+        for(int i = 2; i <= limit; i++) {
+            if(numberList[i]) {
+                primeList.add(i);
+            }
+        }
+
+        return primeList;
+    }
+
 }
